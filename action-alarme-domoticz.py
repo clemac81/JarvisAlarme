fdfae3f9ba53895ddf51065011f397cdeca42579
clemac81 <50763679+clemac81@users.noreply.github.com>
2019-09-22 17:08:46 +0200

    Update action-alarme-domoticz.py

--- action-alarme-domoticz.py
+++ action-alarme-domoticz.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Author: Eric Vandecasteele 2018
-# http://blog.onlinux.fr
-#
-#
 # Import required Python libraries
-import os
 import requests
 r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
 print(r.json)
