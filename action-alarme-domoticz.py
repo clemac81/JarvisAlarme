3ee65d570211c2760a620a12fc40ee50832ee168
clemac81 <50763679+clemac81@users.noreply.github.com>
2019-09-28 10:21:39 +0200

    Update action-alarme-domoticz.py

--- action-alarme-domoticz.py
+++ action-alarme-domoticz.py
@@ -1,5 +1,58 @@
 #!/usr/bin/env python3
-# Import required Python libraries
+# -*- coding: utf-8 -*-
+
+
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+def subscribe_intent_callback(hermes, intentMessage):
+        action_wrapper(hermes, intentMessage)
+
+
+def action_wrapper(hermes, intentMessage):
+    if intentName == "ActivationAlarme":
+	r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
+	print(r.json)}
+    
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+import urllib2
+import json
+import io
+import jellyfish
+from hermes_python.hermes import Hermes
+import configparser
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
 import requests
-r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
-print(r.json)
+
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+MAX_JARO_DISTANCE = 0.4
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def sceneOn_received(hermes, intent_message):
+	r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
+	print(r.json)
+	hermes.publish_end_session(intent_message.session_id, "I'm sorry, I couldn't find a scene like "+lowest_name)
+
+if __name__ == "__main__":
+    global_conf = read_configuration_file(CONFIG_INI)
+    domoticz_base_url = global_conf.get("secret").get("domoticz url")
+    with Hermes('localhost:1883') as h:
+        h.subscribe_intent("iMartyn:listScenes",listScenes_received) \
+         .subscribe_intent("iMartyn:sceneOn",sceneOn_received) \
+         .subscribe_intent("iMartyn:listSwitches",listSwitches_received) \
+         .start()
+
+
