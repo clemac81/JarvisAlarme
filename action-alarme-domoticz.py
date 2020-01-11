103c8d8d7c3d3eb03216b5d0701091ddcfc412e1
clemac81 <50763679+clemac81@users.noreply.github.com>
2020-01-11 21:14:59 +0100

    Update action-alarme-domoticz.py

--- action-alarme-domoticz.py
+++ action-alarme-domoticz.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-import ConfigParser
-import urllib2
-import json
-import io
-import jellyfish
 from hermes_python.hermes import Hermes
-import configparser
-from hermes_python.ffi.utils import MqttOptions
-from hermes_python.ontology import *
-import io
+from datetime import datetime
+from pytz import timezone
 import requests
 
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
-MAX_JARO_DISTANCE = 0.4
 
-if intentName == 'Clemac81:ActivationAlarme':
-    r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
-    print(r.json)
-current_session_id = intentMessage.session_id
-hermes.publish_end_session(current_session_id, result_sentence)
+def intent_received(hermes, intent_message):
+
+	print()
+	print(intent_message.intent.intent_name)
+	print ()
+
+	if intent_message.intent.intent_name == 'Clemac81:ActivationAlarme':
+        r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
+        print(r.json)
+		
+
+		# hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
+		hermes.publish_end_session(intent_message.session_id, sentence)
+
+
+
+
+with Hermes(MQTT_ADDR) as h:
+	h.subscribe_intents(intent_received).start()
+
+#if intentName == 
+#r = requests.get("http://192.168.1.38:8080/json.htm?type=command&param=switchlight&idx=119&switchcmd=On")
+#    print(r.json)
+#current_session_id = intentMessage.session_id
+#hermes.publish_end_session(current_session_id, result_sentence)
 
